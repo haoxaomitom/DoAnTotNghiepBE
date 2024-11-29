37eb65b2bf3e489f99a3dd72ecfbee682446339f
Nguyen Duc Hao <haoxaomitom@gmail.com>
2024-11-29 21:29:05 +0700

    Fix bug

--- src/main/java/com/example/doantotnghiepbe/entity/Users.java
+++ src/main/java/com/example/doantotnghiepbe/entity/Users.java
@@ -80,6 +80,9 @@ public class Users implements UserDetails {
     @JsonBackReference
     private Roles roles;
 
+    @Column(name = "facebook")
+    private String facebook;
+
     @Column(name = "token_verified")
     private String tokenVerified;
 
